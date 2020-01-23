7dc50d200a077f64f9d5b22e79a1fdaea1f1eb04
Ke Yang <keyang@Kes-MacBook-Pro.local>
2020-01-23 15:52:08 -0600

    update

---
+++ precache-manifest.207ea59000f8ab62a811142f26d8aa8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87756ff4c7f4b2f56e57bf9ec53cbf68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93e946091885de8af166",
+    "url": "/static/css/2.ac37e042.chunk.css"
+  },
+  {
+    "revision": "c3d331b6f6d16ffbdf09",
+    "url": "/static/css/main.e3c7e5e0.chunk.css"
+  },
+  {
+    "revision": "93e946091885de8af166",
+    "url": "/static/js/2.7b48334d.chunk.js"
+  },
+  {
+    "revision": "c3d331b6f6d16ffbdf09",
+    "url": "/static/js/main.efd5370b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
